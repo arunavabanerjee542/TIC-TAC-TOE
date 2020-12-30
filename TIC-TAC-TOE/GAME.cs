91db4a0d141be15b9ce992f198331e04cc7735fd
Arunava Banerjee <Arunava Banerjee@DESKTOP-RBAIMPE>
2020-12-30 06:37:26 -0800

    WORKING FINE

--- TIC-TAC-TOE/GAME.cs
+++ TIC-TAC-TOE/GAME.cs
@@ -27,10 +27,14 @@ namespace TIC_TAC_TOE
            
             while(count<9)
             {
+
                 
+
 
                if(count%2==0)
                 {
+                    CheckSpacesAvailable(p1);
+
                     Console.WriteLine(" Player 1 turn ");
                    var mov = p1.MakeMove(board);
                     tracker.UpdateBoard(mov, 1, board);
@@ -46,6 +50,8 @@ namespace TIC_TAC_TOE
 
                 else
                 {
+                    CheckSpacesAvailable(p2);
+
                     Console.WriteLine(" Player 2 turn ");
                     var mov = p2.MakeMove(board);
                     tracker.UpdateBoard(mov, 2, board);
@@ -69,6 +75,26 @@ namespace TIC_TAC_TOE
 
 
         }
+
+
+
+        public void CheckSpacesAvailable(Player p)
+        {
+            Console.WriteLine(" WANT TO KNOW THE AVAILABLE SPACES ");
+            Console.WriteLine(" 1--> YES ");
+            Console.WriteLine(" 2 ---> NO ");
+
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+            if (choice == 1)
+            {
+                p.ViewGridSpacesAvailable(board);
+            }
+
+        }
+
+
+
 
     }
 }
--- TIC-TAC-TOE/SystemUser.cs
+++ TIC-TAC-TOE/SystemUser.cs
@@ -8,8 +8,31 @@ namespace TIC_TAC_TOE
 {
     class SystemUser : Person
     {
-        public void ViewGridSpacesAvailable()
+        public void ViewGridSpacesAvailable(int [,] board)
         {
+            Console.WriteLine(" N/A ---> NOT AVAILABLE ");
+            Console.WriteLine(" A ---> AVAILABLE ");
+            Console.WriteLine("----------------------------------------");
+            
+           for(int i=0;i<3;i++)
+            {
+                for(int j=0;j<3;j++)
+                {
+                    if(board[i,j] == 0)
+                    {
+                        Console.Write("("+i +","+j+")" + "A   ");
+                       
+                    }
+                    else
+                    {
+                        Console.Write("(" + i + "," + j + ")" + "N/A   ");
+                    }
+                }
+                Console.WriteLine();
+
+            }
+            Console.WriteLine("-------------------------------------");
+
 
 
         }
